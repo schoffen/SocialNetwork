9fedd8b8bbabf1ace0a33531531af720573fb3f5
Felipe <fschoffen@yahoo.com>
2023-02-27 22:46:23 -0300

    create main bottom navigation with custom selection

--- app/src/main/java/site/felipeschoffen/socialnetwork/main/MainActivity.kt
+++ app/src/main/java/site/felipeschoffen/socialnetwork/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package site.felipeschoffen.socialnetwork
+package site.felipeschoffen.socialnetwork.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import site.felipeschoffen.socialnetwork.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/site/felipeschoffen/socialnetwork/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package site.felipeschoffen.socialnetwork.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import site.felipeschoffen.socialnetwork.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
