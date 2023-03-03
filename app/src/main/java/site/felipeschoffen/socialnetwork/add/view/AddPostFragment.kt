6633941d12f7935e3e1cbd7900ed4b83bfd238f1
Felipe <fschoffen@yahoo.com>
2023-03-03 12:03:58 -0300

    all add photo fragments

---
+++ app/src/main/java/site/felipeschoffen/socialnetwork/add/view/AddPostFragment.kt
@@ -0,0 +1,20 @@
+package site.felipeschoffen.socialnetwork.add.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import site.felipeschoffen.socialnetwork.R
+
+class AddPostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_post, container, false)
+    }
+
+}
